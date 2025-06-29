26158843a765be8dd7e16edd3522c0faba7b1fae
Inteside <2373850947@qq.com>
2025-06-29 10:45:27 +0800

    update: 添加单元测试

--- src/api/receive.rs
+++ src/api/receive.rs
@@ -20,7 +20,11 @@ pub async fn receive(params: Receive) {
         body: serde_json::to_string(&params.body).unwrap(),
     };
     let result = request.request(request_body).await;
-    println!("result: {:?}", result);
+    if result.is_err() {
+        println!("请求失败: {:?}", result.err());
+    } else {
+        println!("请求成功: {:?}", result.unwrap());
+    }
 }
 
 #[cfg(test)]
@@ -28,20 +32,21 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn test_receive() {
-        let mut header = HeaderMap::new();
-        header.insert("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36".parse().unwrap());
-        header.insert("Authorization", "d8AZUpWrOsfV1GUfqhPS4EQ08BfnRuJ2xIRU0hrPRCD9l32AHpr5QgqtPysy5y_cLJ5vuDm34Cwj2fltIbFO6HFfVzG85e550Q2svY1FfVisWsScTsPNhF89U7XCb7Tp6UWvv2SAq22V2NfQW17DZUC8MNXD".parse().unwrap());
-        let body = BodyConfig {
-            communityId: "54".to_string(),
-            redPackTaskId: "66578".to_string(),
-            zoneId: "214".to_string(),
+    // 测试其他url
+    async fn test_other_url() {
+        let request = request::Request::new();
+        let request_body = request::RequestBody {
+            url: "https://www.google.com/".to_string(),
+            method: Method::GET,
+            headers: HeaderMap::new(),
+            body: "".to_string(),
         };
-        let params = Receive {
-            header: header,
-            body: body,
-        };
-        receive(params).await;
+        let result = request.request(request_body).await;
+        if result.is_err() {
+            println!("请求失败: {:?}", result.err());
+        } else {
+            println!("请求成功: {:?}", result.unwrap());
+        }
     }
 }
 
